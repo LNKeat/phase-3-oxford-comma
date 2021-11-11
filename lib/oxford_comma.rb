1bdd04a2b7bc07359cc3d8346e0b8888de359ce0
Laura <lnkeat@gmail.com>
2021-11-11 11:05:20 -0700

    commit tree 167d0dd78be453ebaf0a68b9773fe710c1425a83

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,31 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length > 2
+    lastWord = array.pop
+    finalStr = array.join(", ") + ", and #{lastWord}"
+  else
+    "This array is empty"
+  end
 end
+
+arr1 = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
+arr2 = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"]
+arr3 = ["kiwi"]
+arr4 = ["kiwi", "durian"]
+
+
+
+def oxford_comma2(array)
+  return array.join(" and ") if array.size < 3
+
+  array[-1] = "and #{array[-1]}"
+
+  array.join(", ")  
+end
+
+
